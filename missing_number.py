ec5ddd4d4c7e5a91d341b3c0aff05a42262c881e
LENNY\kayuyu <mwaurakayuyu@gmail.com>
2017-02-09 10:28:02 +0300

    fix error

--- missing_number.py
+++ missing_number.py
@@ -3,6 +3,10 @@ def missing_number(array1, array2):
 	array2 = set(array2)
 	if array1 == array2:
 		return 0
-
-	 
+		if array1 != array2:
+			array3 = list(array2 - array1)
+			for value in array3:
+				return value
+			else:
+				False
 	
